76214c3e6155e702860067360aca574622b6942a
mariagsmn <mariagassmann0@gmail.com>
2022-11-17 15:29:50 +0100

    kp man

---
+++ EIA_Abgabe_6/my-script.ts
@@ -0,0 +1,7 @@
+const myname = "Maria Gassmann";
+const age = 20;
+
+ console.log("Hallo mein Name ist " + myname + " und ich bin " + age + " Jahre alt.");
+ // Output: Hallo mein Name ist Max Mustermann und ich bin 22 Jahre alt.
+
+
