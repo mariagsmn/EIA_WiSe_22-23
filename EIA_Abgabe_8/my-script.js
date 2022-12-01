302a3d0b668532a083918c3b6b1acffec15c5702
mariagsmn <mariagassmann0@gmail.com>
2022-12-01 15:28:25 +0100

    Abgabe_8

---
+++ EIA_Abgabe_8/my-script.js
@@ -0,0 +1,19 @@
+var sound = new Audio('A.mp3');
+sound.play();
+var sound = new Audio('C.mp3');
+sound.play();
+var sound = new Audio('F.mp3');
+sound.play();
+var sound = new Audio('G.mp3');
+sound.play();
+var sound = new Audio('hihat.mp3');
+sound.play();
+var sound = new Audio('kick.mp3');
+sound.play();
+var sound = new Audio('laugh-1.mp3');
+sound.play();
+var sound = new Audio('laugh-2.mp3');
+sound.play();
+var sound = new Audio('snare.mp3');
+sound.play();
+//# sourceMappingURL=my-script.js.map
---
+++ EIA_Abgabe_8/my-script.ts
@@ -0,0 +1,10 @@
+var sound:HTMLAudioElement = new Audio ('A.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('C.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('F.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('G.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('hihat.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('kick.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('laugh-1.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('laugh-2.mp3'); sound.play();
+var sound:HTMLAudioElement = new Audio ('snare.mp3'); sound.play();
+
