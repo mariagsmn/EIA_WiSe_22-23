d9ce0e347be7b76f22814125c91340b331d29878
mariagsmn <mariagassmann0@gmail.com>
2022-12-14 17:43:45 +0100

    Abgabe_9_überarbeitet

--- EIA_Abgabe_9/my-script.ts
+++ EIA_Abgabe_9/my-script.ts
@@ -93,7 +93,7 @@ namespace a09 {
     remixButton.addEventListener('click', function () {
         let remix: HTMLAudioElement[] = []
         for (let index = 0; index < 3; index++) {
-            const randomNumber: Number = Math.round(Math.random()*9)     
+            const randomNumber: Number = Math.round(Math.random()*8)     
             remix.push(allSounds[randomNumber.toString()])
         }
         beat = remix
